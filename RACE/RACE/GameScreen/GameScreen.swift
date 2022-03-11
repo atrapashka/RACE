49af8fec797fbe5db1795550c3f3da74cf8a4267
atrapashka <a.trapashka@gmail.com>
2022-03-12 01:11:21 +0400

    add blur on start screen

--- RACE/RACE/GameScreen/GameScreen.swift
+++ RACE/RACE/GameScreen/GameScreen.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class GameScreen: UIViewController {
+    
+    private var markupCenter: UIView!
+    private var markupLeftSide: UIView!
+    private var markupRightSide: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray
+        markupSettings()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func markupSettings() {
+        var width: CGFloat = 20
+        var height: CGFloat = 200
+        
+        markupCenter = UIView()
+        markupCenter.backgroundColor = .white
+        markupCenter.frame = CGRect(x: UIScreen.main.bounds.midX - width / 2,
+                              y: UIScreen.main.bounds.midY - height / 2,
+                              width: width,
+                              height: height)
+        
+        markupLeftSide = UIView()
+        markupLeftSide.backgroundColor = .white
+        markupLeftSide.frame = CGRect(x: UIScreen.main.bounds.minX + width / 2,
+                                      y: UIScreen.main.bounds.minY,
+                                      width: width,
+                                      height: UIScreen.main.bounds.height)
+        
+        markupRightSide = UIView()
+        markupRightSide.backgroundColor = .white
+        markupRightSide.frame = CGRect(x: UIScreen.main.bounds.maxX - width * 1.5,
+                                      y: UIScreen.main.bounds.minY,
+                                      width: width,
+                                      height: UIScreen.main.bounds.height)
+        
+        view.addSubview(markupLeftSide)
+        view.addSubview(markupRightSide)
+        view.addSubview(markupCenter)
     }
-    */
-
 }
--- RACE/RACE/ViewController.swift
+++ RACE/RACE/ViewController.swift
@@ -7,41 +7,91 @@
 
 import UIKit
 
-private var width: CGFloat = 350
-private var height: CGFloat = 350
-private var customView: UIView!
+private var width: CGFloat = 500
+private var height: CGFloat = 500
 
 class ViewController: UIViewController {
     @IBOutlet weak var blurView: UIView!
-    @IBOutlet weak var mainBlurView: UIView!
+    @IBOutlet weak var blurViewSecond: UIView!
+    @IBOutlet weak var blurViewThird: UIView!
+    @IBOutlet weak var blurViewFourth: UIView!
+    @IBOutlet weak var mainBlurView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray5
+        view.backgroundColor = .white
+        
+        
         
         blurElementsSettings()
-        
+        addBlurView()
+    }
+    @IBAction func onBurnoutButon(_ sender: Any) {
+        transitToGameScreen()
     }
     
     private func blurElementsSettings() {
         blurView.frame = CGRect(x: UIScreen.main.bounds.midX - width,
                                 y: UIScreen.main.bounds.midY - height * 1.5,
-                                              width: width,
-                                              height: height)
+                                width: width,
+                                height: height)
         blurView.layer.cornerRadius = width / 2
-        
-        UIView.animate(withDuration: 3, delay: 0, options: [.autoreverse, .repeat]) {
+        UIView.animate(withDuration: 5, delay: 0, options: [.autoreverse, .repeat]) {
             self.blurView.frame = CGRect(x: UIScreen.main.bounds.midX + width / 4,
                                          y: UIScreen.main.bounds.midY - height * 1.5,
+                                         width: width,
+                                         height: height)
+        }
+        
+        blurViewSecond.frame = CGRect(x: UIScreen.main.bounds.midX + width / 4,
+                                      y: UIScreen.main.bounds.midY - height,
                                       width: width,
                                       height: height)
+        blurViewSecond.layer.cornerRadius = width / 2
+        UIView.animate(withDuration: 5, delay: 0, options: [.autoreverse, .repeat]) {
+            self.blurViewSecond.frame = CGRect(x: UIScreen.main.bounds.midX - width,
+                                               y: UIScreen.main.bounds.midY - height,
+                                               width: width,
+                                               height: height)
+        }
+        
+        blurViewThird.frame = CGRect(x: UIScreen.main.bounds.midX - width,
+                                     y: UIScreen.main.bounds.midY - height / 2,
+                                      width: width,
+                                      height: height)
+        blurViewThird.layer.cornerRadius = width / 2
+        UIView.animate(withDuration: 5, delay: 0, options: [.autoreverse, .repeat]) {
+            self.blurViewThird.frame = CGRect(x: UIScreen.main.bounds.midX + width / 4,
+                                              y: UIScreen.main.bounds.midY - height / 2,
+                                               width: width,
+                                               height: height)
+        }
+        
+        blurViewFourth.frame = CGRect(x: UIScreen.main.bounds.midX + width / 4,
+                                      y: UIScreen.main.bounds.midY,
+                                      width: width,
+                                      height: height)
+        blurViewFourth.layer.cornerRadius = width / 2
+        UIView.animate(withDuration: 5, delay: 0, options: [.autoreverse, .repeat]) {
+            self.blurViewFourth.frame = CGRect(x: UIScreen.main.bounds.midX - width,
+                                               y: UIScreen.main.bounds.midY,
+                                               width: width,
+                                               height: height)
         }
     }
     
     private func addBlurView() {
-        
+        let blur = UIBlurEffect(style: .light)
+        let blurEffectView = UIVisualEffectView(effect: blur)
+        blurEffectView.frame = mainBlurView.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mainBlurView.addSubview(blurEffectView)
     }
-
-
+    
+    func transitToGameScreen() {
+        let storyboard = UIStoryboard.init(name: "GameScreen", bundle: Bundle.main)
+        let vc = storyboard.instantiateInitialViewController() as! GameScreen
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
